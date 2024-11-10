60f892f1c82001c3bd977c2bcada2956c50fc967
zrain03 <150784633+zrain03@users.noreply.github.com>
2024-11-10 10:59:45 +0800

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -1,4 +1,4 @@
-from telethon import TelegramClient
+from telethon import TelegramClient, errors
 import asyncio
 import os
 
@@ -8,7 +8,7 @@ api_hash = os.getenv("API_HASH")
 session_name = os.getenv("SESSION_NAME")  # Nama fail sesi yang digunakan
 
 # ID grup dan mesej yang ingin dihantar
-group_id = -10022200241778
+group_id = -10022200241778  # Pastikan ini adalah ID kumpulan yang betul
 message = "Maaf"
 
 # Buat klien Telegram
@@ -17,6 +17,17 @@ client = TelegramClient(session_name, api_id, api_hash)
 # Fungsi untuk menghantar mesej beberapa kali
 async def send_message_repeatedly():
     await client.start()
+    try:
+        # Cuba untuk mengakses kumpulan terlebih dahulu
+        await client.get_entity(group_id)
+    except errors.ChatAdminRequiredError:
+        print("Akses ke kumpulan ini tidak dibenarkan.")
+        return
+    except ValueError:
+        print("ID kumpulan tidak sah atau anda tidak mempunyai akses.")
+        return
+
+    # Menghantar mesej jika berjaya mendapat akses ke kumpulan
     for i in range(10000):
         await client.send_message(group_id, message)
         print(f"Mesej ke-{i+1} telah dihantar!")
