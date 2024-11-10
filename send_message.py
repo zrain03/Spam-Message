e4602f462aae2b610f6d35578f31926b4162ff91
zrain03 <150784633+zrain03@users.noreply.github.com>
2024-11-10 11:32:37 +0800

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -2,10 +2,10 @@ from telethon import TelegramClient, errors
 import asyncio
 import os
 
-# Dapatkan API ID, API Hash, dan nama fail sesi dari input atau environment
-api_id = int(input("Masukkan API_ID: "))
-api_hash = input("Masukkan API_HASH: ")
-session_name = "my_session"  # Nama fail sesi yang digunakan
+# Dapatkan API_ID, API_HASH dan nama fail sesi dari environment variables
+api_id = int(os.getenv("API_ID"))
+api_hash = os.getenv("API_HASH")
+session_name = os.getenv("SESSION_NAME", "my_session")  # Nama fail sesi yang digunakan
 
 # ID kumpulan dan mesej yang ingin dihantar
 group_id = -1002200241778  # ID kumpulan "Testing for MP CHAT"
@@ -28,10 +28,10 @@ async def send_message_repeatedly():
         return
 
     # Hantar mesej 100 kali
-    for i in range(100):
+    for i in range(25):
         await client.send_message(group_id, message)
         print(f"Mesej ke-{i+1} telah dihantar!")
-        await asyncio.sleep(1)  # Tunggu 1 saat antara mesej
+        await asyncio.sleep(5)  # Tunggu 1 saat antara mesej
 
 # Jalankan fungsi untuk menghantar mesej
 with client:
