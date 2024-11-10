a6bca843ee82efa61c955dd72c53462b6d4fed85
zrain03 <150784633+zrain03@users.noreply.github.com>
2024-11-10 10:57:57 +0800

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -2,28 +2,26 @@ from telethon import TelegramClient
 import asyncio
 import os
 
-# Mengambil API ID, API Hash, dan sesi dari environment variables
+# Dapatkan API ID dan API Hash daripada GitHub Secrets
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
-session_name = os.getenv("SESSION_NAME")
+session_name = os.getenv("SESSION_NAME")  # Nama fail sesi yang digunakan
 
-# ID grup
+# ID grup dan mesej yang ingin dihantar
 group_id = -10022200241778
-
-# Mesej yang ingin dihantar
 message = "Maaf"
 
-# Membuat klien Telegram
+# Buat klien Telegram
 client = TelegramClient(session_name, api_id, api_hash)
 
-# Fungsi untuk mengirim pesan beberapa kali dengan jeda
+# Fungsi untuk menghantar mesej beberapa kali
 async def send_message_repeatedly():
     await client.start()
     for i in range(10000):
         await client.send_message(group_id, message)
         print(f"Mesej ke-{i+1} telah dihantar!")
-        await asyncio.sleep(1)  # Jeda 1 detik antara setiap pesan
+        await asyncio.sleep(1)
 
-# Jalankan fungsi
+# Jalankan fungsi utama
 with client:
     client.loop.run_until_complete(send_message_repeatedly())
