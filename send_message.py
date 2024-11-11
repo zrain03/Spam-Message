3074810f4c0bbb7b52fe11f0796ae62120de4c86
zrain03 <150784633+zrain03@users.noreply.github.com>
2024-11-11 22:18:25 +0800

    Update send_message.py

--- send_message.py
+++ send_message.py
@@ -8,11 +8,7 @@ api_hash = os.getenv("API_HASH")
 session_name = os.getenv("SESSION_NAME", "my_session")  # Nama fail sesi yang digunakan
 
 # ID kumpulan dan mesej yang ingin dihantar
-group_ids = [-1002200241778, -1002288720559, -1001161916810, -1001897381179, -1002056756757, 
-             -1001656152814, -1001641889050, -1002032128619, -1001513360832, -1002083219543, 
-             -1001338686972, -1002080497857, -1001194015232, -1002080593272, -1001473865431, 
-             -1001778610644, -1002065798504, -1002329458039, -1002379900836, -1001192339217, 
-             -1001875616025, -1001938068566, -1001626914578, -1002174886965, -1001965994500]  # ID kumpulan "Testing for MP CHAT" dan satu lagi kumpulan
+group_ids = [-1002200241778, -1002288720559]  # ID kumpulan "Testing for MP CHAT" dan satu lagi kumpulan
 message = "https://t.me/mpgoviralgrowthtools (admin share 3 kali MP goviral setiap hari)"
 
 # Buat klien Telegram
@@ -33,11 +29,11 @@ async def send_message_repeatedly():
         return
 
     # Hantar mesej kepada setiap kumpulan
-    for i in range(24):
+    for i in range(10):
         for group_id in group_ids:
             await client.send_message(group_id, message)
             print(f"Mesej ke-{i+1} telah dihantar ke kumpulan {group_id}!")
-        await asyncio.sleep(1800)  # Tunggu 3600 saat / 1 jam antara mesej
+        await asyncio.sleep(3)  # Tunggu 3600 saat / 1 jam antara mesej
 
 # Jalankan fungsi untuk menghantar mesej
 with client:
